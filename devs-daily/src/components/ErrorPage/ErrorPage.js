f9bfbfbe72bc49e306b88fcb517b81393de8e878
John Ammon <ammonj78@gmail.com>
2023-04-11 17:09:50 -0600

    added errorpage

---
+++ devs-daily/src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+
+
+const ErrorPage = () => {
+    return (
+        <div className="errorPage">
+            {/* <div className="oops"></div> */}
+            <h1>404 Error</h1>
+            <h2>You Look Lost!</h2>
+            <p>This link will take you back home:</p>
+            <Link to='/'>Home</Link>
+        </div>
+    )
+}
+export default ErrorPage
