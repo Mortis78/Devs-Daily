5650f6eaf0f5397f66849dd826fad998d14ae491
John Ammon <ammonj78@gmail.com>
2023-04-11 16:07:27 -0600

    Added basic card

---
+++ devs-daily/src/components/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Card.css';
+
+const Card = (props) => {
+  return (
+
+    <section>
+        {/* <Link className='' to={`/${props.id}`}>
+            <span className=''>
+                <img className="" src={props.} alt={'img'} />
+                <h3 className=''>{props.}</h3>
+            </span>
+        </Link>
+        <span className=''>
+            <p>{props.}</p>
+        </span> */}
+    </section>
+    )
+}
