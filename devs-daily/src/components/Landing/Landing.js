b51ff43d8c218a61e30e0fd1a2ce4a62e8a24eeb
John Ammon <ammonj78@gmail.com>
2023-04-11 16:07:56 -0600

    Added basic landing

---
+++ devs-daily/src/components/Landing/Landing.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Card from '../Card/Card'
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+import './Landing.css'
+
+const Landing = (props) => {
+
+  const articleCards = props.articles.map(article => {
+    return (
+      <Card
+        // id={article.id}
+        // key={article.id}
+        // title={article.title}
+        // description={article.description}
+    
+      />
+    )
+  })
+
+  return (
+    <div className='article-cards'>
+      <Link className='favorites-link' to={'/Favorites'}>
+        Go to Favorites
+        </Link>
+      {articleCards}
+    </div>
+  )
+}
+
+export default Landing
