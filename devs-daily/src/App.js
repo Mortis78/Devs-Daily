a254eb96d68fed99d20d7fce42d17f6f1e09bce3
John Ammon <ammonj78@gmail.com>
2023-04-11 18:01:43 -0600

    Added imports for App

--- devs-daily/src/App.js
+++ devs-daily/src/App.js
@@ -1,5 +1,8 @@
 
 import './App.css';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer'
+import { ErrorPage } from './components/ErrorPage/ErrorPage';
 
 export function App() {
   return (
